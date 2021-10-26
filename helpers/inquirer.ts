2e8f284f7a6cdf7d85ad54f3ca6043cd91110c9d
joelgrod <joelgrod@gmail.com>
2021-10-26 19:00:38 +0100

    delete task

--- helpers/inquirer.ts
+++ helpers/inquirer.ts
@@ -1,127 +1,121 @@
 import { Task } from "../models/Task";
 
-var inquirer = require('inquirer');
+var inquirer = require("inquirer");
 
 const questions = [
-    {
-        type: "list",
-        name: "option",
-        message: "What goes next?",
-        choices: [
-            {
-                name:`${"1.".green} Create task`,
-                value: "1"
-            },
-            {
-                name:`${"2.".green} List tasks`,
-                value: "2"
-            },
-            {
-                name:`${"3.".green} List completed tasks`,
-                value: "3"
-            },
-            {
-                name:`${"4.".green} List pending tasks`,
-                value: "4"
-            },
-            {
-                name:`${"5.".green} Complete task(s)`,
-                value: "5"
-            },
-            {
-                name:`${"6.".green} Delete task`,
-                value: "6"
-            },
-            {
-                name:`${"0.".green} Exit`,
-                value: "0"
-            },
-        ]
-    }
+  {
+    type: "list",
+    name: "option",
+    message: "What goes next?",
+    choices: [
+      {
+        name: `${"1.".green} Create task`,
+        value: "1",
+      },
+      {
+        name: `${"2.".green} List tasks`,
+        value: "2",
+      },
+      {
+        name: `${"3.".green} List completed tasks`,
+        value: "3",
+      },
+      {
+        name: `${"4.".green} List pending tasks`,
+        value: "4",
+      },
+      {
+        name: `${"5.".green} Complete task(s)`,
+        value: "5",
+      },
+      {
+        name: `${"6.".green} Delete task`,
+        value: "6",
+      },
+      {
+        name: `${"0.".green} Exit`,
+        value: "0",
+      },
+    ],
+  },
 ];
 
 const pausePrompt = [
-    {
-        type: "input",
-        name: "input",
-        message: `Press ${"Enter".green} to continue`
-    }
+  {
+    type: "input",
+    name: "input",
+    message: `Press ${"Enter".green} to continue`,
+  },
 ];
 
 export const inquirerMenu = async () => {
-    console.clear();
-    console.log("========================".green);
-    console.log("    Select an option    ".white);
-    console.log("========================".green);
+  console.clear();
+  console.log("========================".green);
+  console.log("    Select an option    ".white);
+  console.log("========================".green);
 
-    const { option } = await inquirer.prompt(questions);
-    return option;
-}
+  const { option } = await inquirer.prompt(questions);
+  return option;
+};
 
 export const pause = async () => {
-    console.log("\n");
-    await inquirer.prompt(pausePrompt);
-}
+  console.log("\n");
+  await inquirer.prompt(pausePrompt);
+};
 
-export const readInput = async ( message: string ) => {
-    const question = [
-        {
-            type: "input",
-            name: "desc",
-            message,
-            validate( value: string ) {
-                if( value.length === 0 ) {
-                    return "Please, Enter a value";
-                };
-                return true;
-            }
+export const readInput = async (message: string) => {
+  const question = [
+    {
+      type: "input",
+      name: "desc",
+      message,
+      validate(value: string) {
+        if (value.length === 0) {
+          return "Please, Enter a value";
         }
-    ];
-    const { desc } = await inquirer.prompt(question);
-    return desc;
-}
+        return true;
+      },
+    },
+  ];
+  const { desc } = await inquirer.prompt(question);
+  return desc;
+};
 
-export const deleteTasks = async ( taskList: Task[] ) => {
+export const deleteTasks = async (taskList: Task[]) => {
+  const choices = taskList.map((task, idx) => {
+    const id = `${idx + 1}.`.green;
+    return {
+      name: `${id} ${task.desc}`,
+      value: task.id,
+    };
+  });
 
-    const choices = taskList.map( ( task, idx ) => {
-        const id = `${ idx + 1 }.`.green;
-        return {
-            name: `${ id } ${ task.desc }`,
-            value: task.id
-        }
-    });
+  choices.unshift({
+    name: "0. ".green + "Cancel",
+    value: "0",
+  });
 
-    const questions = [
-        {
-            type: "list",
-            name: "id",
-            message: "What task do you want to delete?",
-            choices
-        }
-    ];
-    
-    const { id } = await inquirer.prompt(questions);
-    return id;
-}
+  const questions = [
+    {
+      type: "list",
+      name: "id",
+      message: "What task do you want to delete?",
+      choices,
+    },
+  ];
 
-export const confirmation = async ( message: string ) => {
-    const questions = [
-        {
-            type: "list",
-            name: "confirm",
-            message,
-            choices: [
-                {
-                    name: "Yes",
-                    value: true
-                },
-                {
-                    name: "No",
-                    value: false
-                }
-            ]
-        }
-    ];
-    const { confirm } = await inquirer.prompt(questions);
-    return confirm;
-}
+  const { id } = await inquirer.prompt(questions);
+  return id;
+};
+
+export const confirmation = async (message: string) => {
+  const questions = [
+    {
+      type: "confirm",
+      name: "ok",
+      message,
+    },
+  ];
+  const { ok } = await inquirer.prompt(questions);
+  return ok;
+};
--- index.ts
+++ index.ts
@@ -1,71 +1,74 @@
 import colors from "colors";
 
 // import { pause, showMenu } from "./helpers/messages";
-import { 
-    confirmation,
-    deleteTasks,
-    inquirerMenu, 
-    pause, 
-    readInput 
-} from './helpers/inquirer';
+import {
+  confirmation,
+  deleteTasks,
+  inquirerMenu,
+  pause,
+  readInput,
+} from "./helpers/inquirer";
 import { TodoList } from "./models/TodoList";
-import { saveData, readData } from './helpers/dataHandler';
+import { saveData, readData } from "./helpers/dataHandler";
 
 console.clear();
 colors; // Creates new string prototype that extends from the original
 
-const options = ( todoList: TodoList ): { [id: string ]: Function }  => {
-    return {
-        "1": async () => {
-            const desc = await readInput("Task description:");
-            todoList.createTask(desc);
-        },
-        "2": () => {
-            console.log();
-            todoList.showTasks( todoList.list );
-        },
-        "3": () => {
-            todoList.showPendingCompletedTasks(true);
-        },
-        "4": () => {
-            todoList.showPendingCompletedTasks(false);
-        },
-        "6": async () => {
-            const id = await deleteTasks( todoList.list );
-            const confirm = await confirmation( "Are you sure?" );
-            if( confirm ) todoList.deleteTask(id);
-        },
-    }
-}
+const options = (todoList: TodoList): { [id: string]: Function } => {
+  return {
+    "1": async () => {
+      const desc = await readInput("Task description:");
+      todoList.createTask(desc);
+    },
+    "2": () => {
+      console.log();
+      todoList.showTasks(todoList.list);
+    },
+    "3": () => {
+      todoList.showPendingCompletedTasks(true);
+    },
+    "4": () => {
+      todoList.showPendingCompletedTasks(false);
+    },
+    "6": async () => {
+      const id = await deleteTasks(todoList.list);
+      if (id === "0") return;
+      const confirm = await confirmation("Are you sure?");
+      if (confirm) {
+        todoList.deleteTask(id);
+        console.log("Task deleted");
+      }
+    },
+  };
+};
 
 const main = async () => {
-    // console.log("Hello World".green);
+  // console.log("Hello World".green);
 
-    let resp: string = "";
-    const todoList = new TodoList();
+  let resp: string = "";
+  const todoList = new TodoList();
 
-    const dataDB = readData();
-    if( dataDB ) todoList.saveArrayData(dataDB);
-    
-    do {
-        resp = await inquirerMenu();
+  const dataDB = readData();
+  if (dataDB) todoList.saveArrayData(dataDB);
 
-        if(typeof options(todoList)[resp] === "function")
-            await options(todoList)[resp]();
-        
-        saveData( todoList.list );
-        
-        if( resp !== "0" ) await pause();
-    } while ( resp !== "0" );
+  do {
+    resp = await inquirerMenu();
 
-    /** Delete this */
-    // Manual Menu
-    // do {
-    //     resp = await showMenu();
-    //     console.log(resp);
-    //     if( resp !== "0" ) await pause();
-    // } while (resp !== "0");
+    if (typeof options(todoList)[resp] === "function")
+      await options(todoList)[resp]();
 
-}
+    saveData(todoList.list);
+
+    if (resp !== "0") await pause();
+  } while (resp !== "0");
+
+  /** Delete this */
+  // Manual Menu
+  // do {
+  //     resp = await showMenu();
+  //     console.log(resp);
+  //     if( resp !== "0" ) await pause();
+  // } while (resp !== "0");
+};
 
 main();
